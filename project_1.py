cf108e613ada121c68748d87a3596b07fb520a1c
Neethipriya S <116978826+Neethi-priya@users.noreply.github.com>
2022-12-18 01:33:01 +0530

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,43 @@
+class Project_Data:
+    username = "Admin"
+    password = "admin123" 
+    wrong_password = "Invalid password"
+    
+    employee_firstname='Neethu'
+    employee_middlename='priya'
+    employee_lastname='Selvaraj'
+    emp_id='987789128'
+    emp_name = 'Neethu priya Selvaraj'
+    dob='1997-12-21'
+
+class Project_Selectors:
+    input_box_username = "username"
+    input_box_password = "password"
+    login_xpath = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button' 
+    
+    pim_xpath = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a' 
+    add_employee_xpath='/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[3]/a' 
+    
+    employee_box_firstname = 'firstName'
+    employee_box_middlename= 'middleName'
+    employee_box_lastname= 'lastName' 
+    employee_id_box='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input'
+    
+    save_button = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]'
+    
+    employee_list_xpath='//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]' 
+    
+    employee_list_names_xpath ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[45]/div/div[3]/div'
+    
+    employee_name ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input'
+    employee_id_box_2='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input'
+    search_xpath='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]' 
+    profile_click_xpath ='/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button[2]/i' 
+    save_button_edit_employee='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
+    
+    deleting_button ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button[1]'
+    deleting_button_confirmation='//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]' 
+    dob_field ='/html/body/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/div/input'
+    bullet_button ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/label/span'
+    
+
